381b2a1284150577165a5730fc974d3d0bc7b942
Dougal Houston <dhouston@caci.co.uk>
2023-12-05 10:59:04 +0000

    Day 5 slight optimisation

--- src/main/java/solutions/Day05.java
+++ src/main/java/solutions/Day05.java
@@ -2,7 +2,6 @@ package solutions;
 
 import Runner.Answer;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,22 +9,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public class Day05 implements Day {
     private static final Logger logger = Logger.getLogger(Day05.class.getSimpleName());
 
     public void part1(List<String> input) {
         final String seeds = input.remove(0);
-        final List<BigInteger> allSeeds = getSeeds(seeds);
+        final List<Long> allSeeds = getSeeds(seeds);
         final Map<Conversion, SeedMap> maps = splitInput(input);
 
-        final List<BigInteger> locations = new ArrayList<>();
-        for (BigInteger i : allSeeds) {
+        final List<Long> locations = new ArrayList<>();
+        for (Long i : allSeeds) {
             Conversion current = Conversion.SEED;
-            BigInteger currentValue = i;
+            Long currentValue = i;
             while (!current.equals(Conversion.LOCATION)) {
                 final SeedMap currentSeedMap = maps.get(current);
                 currentValue = currentSeedMap.convertValue(currentValue);
@@ -35,24 +32,21 @@ public class Day05 implements Day {
             locations.add(currentValue);
         }
 
-        final BigInteger minimum = Collections.min(locations);
+        final Long minimum = Collections.min(locations);
         logger.log(Answer.ANSWER, String.valueOf(minimum));
     }
 
     public void part2(List<String> input) {
         final String seeds = input.remove(0);
-        final List<BigInteger> allSeeds = getSeeds(seeds);
+        final List<Long> allSeeds = getSeeds(seeds);
         final Map<Conversion, SeedMap> maps = splitInput(input, true);
-
-        BigInteger currentLocation = new BigInteger("0");
-        BigInteger maxLocation = new BigInteger("178159714");
+        final Long maxLocation = 178159714L;
+        Long currentLocation = 0L;
 
         while (currentLocation.compareTo(maxLocation) < 0) {
-            if (currentLocation.mod(BigInteger.valueOf(1000000L)).equals(BigInteger.ZERO)) {
-                System.out.println(String.valueOf(currentLocation));
-            }
             Conversion currentType = Conversion.LOCATION;
-            BigInteger currentValue = currentLocation;
+            Long currentValue = currentLocation;
+
             while (!currentType.equals(Conversion.SEED)) {
                 final SeedMap currentSeedMap = maps.get(currentType);
                 currentValue = currentSeedMap.reverseConvert(currentValue);
@@ -63,50 +57,26 @@ public class Day05 implements Day {
                 logger.log(Answer.ANSWER, String.valueOf(currentLocation));
                 System.exit(0);
             }
-            currentLocation = currentLocation.add(BigInteger.valueOf(1L));
+            
+            currentLocation += 1L;
         }
-//
-//        for
-
-//        for (int i = 0; i < allSeeds.size(); i += 2) {
-//            System.out.println(String.format("%d of %d", i, allSeeds.size()));
-//            BigInteger current = allSeeds.get(i);
-//            BigInteger max = current.add(allSeeds.get(i + 1));
-//
-//            while (current.compareTo(max) < 0) {
-//                Conversion currentType = Conversion.SEED;
-//                BigInteger currentValue = current;
-//                while (!currentType.equals(Conversion.LOCATION)) {
-//                    final SeedMap currentSeedMap = maps.get(currentType);
-//                    currentValue = currentSeedMap.convertValue(currentValue);
-//                    currentType = currentSeedMap.target;
-//                }
-//
-//                if (bestLocation == null || currentValue.compareTo(bestLocation) < 0) {
-//                    bestLocation = currentValue;
-//                }
-//                current = current.add(BigInteger.valueOf(1L));
-//            }
-//
-//        }
-
-//        logger.log(Answer.ANSWER, String.valueOf(bestLocation));
     }
 
-    private static boolean isValidSeed(BigInteger seed, List<BigInteger> ranges) {
+    private static boolean isValidSeed(Long seed, List<Long> ranges) {
         for (int i = 0; i < ranges.size(); i += 2) {
-            BigInteger first = ranges.get(i);
-            BigInteger second = ranges.get(i + 1);
-            BigInteger diff = seed.subtract(first);
-            if (diff.compareTo(BigInteger.valueOf(0L)) > 0 && diff.compareTo(second) < 0) {
+            final Long first = ranges.get(i);
+            final Long second = ranges.get(i + 1);
+            final Long diff = seed - first;
+
+            if (diff.compareTo(0L) > 0 && diff.compareTo(second) < 0) {
                 return true;
             }
         }
         return false;
     }
 
-    private static List<BigInteger> getSeeds(String seeds) {
-        return Arrays.stream(seeds.split(": ")[1].split(" ")).map(BigInteger::new).toList();
+    private static List<Long> getSeeds(String seeds) {
+        return Arrays.stream(seeds.split(": ")[1].split(" ")).map(Long::new).toList();
     }
 
     private static Map<Conversion, SeedMap> splitInput(List<String> inputList) {
@@ -114,9 +84,8 @@ public class Day05 implements Day {
     }
 
     private static Map<Conversion, SeedMap> splitInput(List<String> inputList, boolean reverse) {
-        List<String> sublist = new ArrayList<>();
-
-        Map<Conversion, SeedMap> conversionSeedMap = new HashMap<>();
+        final List<String> sublist = new ArrayList<>();
+        final Map<Conversion, SeedMap> conversionSeedMap = new HashMap<>();
 
         for (String str : inputList) {
             if (str.isEmpty()) {
@@ -178,52 +147,52 @@ public class Day05 implements Day {
                     .toList();
         }
 
-        private List<BigInteger> parseMap(String map) {
-            return Arrays.stream(map.split(" ")).map(BigInteger::new).toList();
+        private List<Long> parseMap(String map) {
+            return Arrays.stream(map.split(" ")).map(Long::valueOf).toList();
         }
 
-        public BigInteger convertValue(BigInteger value) {
-            Optional<BigInteger> o = maps.stream().map((SingleMap sm) -> sm.convertValue(value)).filter((BigInteger i) -> i.compareTo(BigInteger.valueOf(-1L)) != 0).findFirst();
+        public Long convertValue(Long value) {
+            final Optional<Long> o = maps.stream().map((SingleMap sm) -> sm.convertValue(value)).filter((Long i) -> i.compareTo(-1L) != 0).findFirst();
 
             return o.orElse(value);
         }
 
-        public BigInteger reverseConvert(BigInteger value) {
-            Optional<BigInteger> o = maps.stream().map((SingleMap sm) -> sm.reverseConvert(value)).filter((BigInteger i) -> i.compareTo(BigInteger.valueOf(-1L)) != 0).findFirst();
+        public Long reverseConvert(Long value) {
+            final Optional<Long> o = maps.stream().map((SingleMap sm) -> sm.reverseConvert(value)).filter((Long i) -> i.compareTo(-1L) != 0).findFirst();
 
             return o.orElse(value);
         }
     }
 
     static class SingleMap {
-        private final BigInteger destStart;
-        private final BigInteger srcStart;
-        private final BigInteger range;
+        private final Long destStart;
+        private final Long srcStart;
+        private final Long range;
 
-        public SingleMap(List<BigInteger> values) {
+        public SingleMap(List<Long> values) {
             destStart = values.get(0);
             srcStart = values.get(1);
             range = values.get(2);
         }
 
-        public BigInteger convertValue(BigInteger value) {
-            final BigInteger diff = value.subtract(srcStart);
+        public Long convertValue(Long value) {
+            final Long diff = value - srcStart;
 
-            if (diff.compareTo(BigInteger.valueOf(0L)) < 0 || diff.compareTo(range) > 0) {
-                return BigInteger.valueOf(-1L);
+            if (diff.compareTo(0L) < 0 || diff.compareTo(range) > 0) {
+                return -1L;
             }
 
-            return destStart.add(diff);
+            return destStart + diff;
         }
 
-        public BigInteger reverseConvert(BigInteger value) {
-            final BigInteger diff = value.subtract(destStart);
+        public Long reverseConvert(Long value) {
+            final Long diff = value - destStart;
 
-            if (diff.compareTo(BigInteger.valueOf(0L)) < 0 || diff.compareTo(range) > 0) {
-                return BigInteger.valueOf(-1L);
+            if (diff.compareTo(0L) < 0 || diff.compareTo(range) > 0) {
+                return -1L;
             }
 
-            return srcStart.add(diff);
+            return srcStart + diff;
         }
     }
 }
